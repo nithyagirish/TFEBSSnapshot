8a675e32b2ed1a2cb9562f14cccf5a32ec3526ca
nithyagirish <120184496+nithyagirish@users.noreply.github.com>
2023-01-17 18:02:45 +0530

    Update snapshot.tf

--- snapshot.tf
+++ snapshot.tf
@@ -2,7 +2,7 @@ resource "aws_ebs_volume" "example" {
   availability_zone = "zone1"
   size              = "size1"
   type              = "type1"
-  encrypted         = "true"
+  encrypted         = "encrypted"
 
   tags = {
     Name = "ebsvolume"
